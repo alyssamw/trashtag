d272f9943a6e65a33841558ab5344f89ecd84611
ellie <ellieyhc@gmail.com>
2019-03-30 12:06:39 -0700

    Facebook login

---
+++ trashtag/src/pages/Feed/Feed.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Feed.css';
+
+class Feed extends Component {
+  render() {
+    return (
+      <div className="Feed">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        <p>This is your feed</p>
+      </div>
+    );
+  }
+}
+
+export default Feed;
